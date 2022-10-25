d2cbca472d853a9ad304a4df66dd5eb88f911a7e
Paulo Deyvid <88635207+PDEYVID@users.noreply.github.com>
2022-10-25 01:04:34 -0300

    Orange

---
+++ 02 - condicionais e loops/aula2.js
@@ -0,0 +1,39 @@
+//variaveis booleanas verdairo ou falsa 1 ou 0 
+/*
+const camisataPauloAzul = true;
+const camisataPauloAzul = false;*/
+
+const numero = 1;
+const isnumeroDivisivelpor5 = (numero % 2) === 0;
+
+//if -> se
+// else if -> se não
+
+if (numero === 0) { // essa condição e verdadeiro
+    console.log("O Numero e Inválido"); // executa isso
+} else if (numero === 5) { // se essa for verdadeira 
+    console.log("O Numero e Inválido"); // executa isso
+} else if (isnumeroDivisivelpor5) { // se essa for verdadeira 
+    console.log("sim");
+} else { // se nem uma for verdadeira 
+    console.log("não"); // executa isso
+}
+
+// condição para numero de par 
+// se o numero par for par
+// 1 iigual e atribuição
+// 2 igual e comparação de String e ele vai converter a string em numero
+// 3 iguais para ver igualdade
+
+/*const NumeroPar = (numero % 2) === 0;
+console.log(NumeroPar);*/
+
+//se o numero par imprimir no console
+/*if (numeroDivisivelpor5) {
+    console.log("sim");
+} else {
+    console.log("não");
+}*/
+// se o numero não for par e impar
+/*if (!NumeroPar) {  
+}*/
---
+++ 02 - condicionais e loops/desafio.js
@@ -0,0 +1,27 @@
+/*Faça um programa para calcular o valor de uma viagem.
+
+Você terá 5 variáveis. Sendo elas:
+ 1 - Preço do etanol;
+ 2 - Preço do gasolina;
+ 3 - O tipo de combustível que está no seu carro;
+ 4 - Gasto médio de combustível do carro por KM;
+ 5 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.*/
+
+
+const precoEtanol = 5.79;
+const precoGasolina = 5.00;
+const kmPorlitros = 10;
+const distanciaEmKm = 100;
+const tipoCombustivel = 'Gasolina';
+
+const litrosCosumidos = distanciaEmKm / kmPorlitros;
+if (tipoCombustivel === 'Gasolina') {
+    let valorGastor = litrosCosumidos * precoGasolina;
+    console.log(valorGastor.toFixed(2));
+} else{
+    let valorGastor = litrosCosumidos * precoGasolina;
+    console.log(valorGastor.toFixed(2));
+}
+
